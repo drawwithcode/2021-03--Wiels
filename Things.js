2b2b6f5262f3e6d0c82cf45e11638789ace19d4d
Wiels <edward.wiels@student.kuleuven.be>
2021-10-25 13:07:56 +0200

    fjieuw

---
+++ Things.js
@@ -0,0 +1,130 @@
+
+class Teksten{
+  constructor(movies){
+    this.rwsize = random(width/8,width/5)
+    this.rhsize = random(height/7,height/4)
+    this.x = random(width)
+    this.y=random(height)
+    this.z=random(height/2)
+    let tekst = createGraphics(this.rwsize,this.rhsize)
+    tekst.fill(color(random(255),random(255),random(255),random(255)))
+    tekst.textAlign(CENTER)
+    tekst.textSize(random(10,50))
+    let size = 500
+    let movie_number = round(random(size-1))
+    let movie = movies[movie_number]
+    let titel = movie.title
+    tekst.text(titel,this.rwsize/2,this.rhsize/2)
+    this.texture = tekst
+
+  }
+
+  display(){
+    texture(this.texture)
+    push()
+    translate(this.x-width/2,this.y-height/2,this.z-height/2)
+    plane(this.rwsize*2,this.rhsize*2)
+    pop()
+  }
+
+}
+
+
+
+class Things{
+  constructor(skins){
+    this.x = random(width)
+    this.y = random(height)
+    this.z_range = height/4
+    this.z = random(this.z_range)
+    this.size = random(1,5)
+    this.stroke_color = color(random(255),random(255),random(255),random(255))
+    this.stroke_weight = random(0,15)
+    this.angle = random(0,0.2)
+    this.skin_number = round(random(skins.length-1))
+    this.skin = skins[this.skin_number]
+    this.perlin = random(0.0001,0.001)
+  }
+
+  move(){
+
+  }
+
+  display(volume){
+    push()
+    translate(this.x-width/2,this.y-height/2,this.z-this.z_range/6)
+    rotateX(this.angle)
+    rotateY(this.angle*0.3)
+    rotateZ(this.angle*0.2)
+    stroke(this.stroke_color)
+    texture(this.skin)
+    strokeWeight(this.stroke_weight/(this.size*3))
+    box(sqrt(volume)*20/this.size)
+    this.angle+=0.005
+    pop()
+
+  }
+
+  change_skin(skins){
+    let random_number = round(random(skins.length-1))
+    this.skin = skins[random_number]
+  }
+
+
+}
+
+
+
+
+/*
+
+
+
+class Things{
+  constructor(skins){
+    this.x = random(width)
+    this.y = random(height)
+    this.z_range = height/2
+    this.z = random(this.z_range)
+    this.offx = random(1000)
+    this.offy = random(20,6000)
+    this.offz = random(5000,100000)
+    this.size = random(1,10)
+
+    this.fill_color = color(random(255),random(255),random(255),random(255))
+    this.stroke_color = color(random(255),random(255),random(255),random(255))
+    this.stroke_weight = random(0,15)
+    this.angle = random(0,0.2)
+    this.skin_number = round(random(skins.length-1))
+    this.skin = skins[this.skin_number]
+    this.perlin = random(0.0001,0.001)
+  }
+
+  move(){
+    this.offx+=this.perlin*2
+    this.offy+=this.perlin*3
+    this.offz+=this.perlin
+    this.x = noise(this.offx)*width
+    this.y = noise(this.offy)*height
+    this.z = noise(this.offz)*this.z_range
+  }
+
+  display(volume){
+    translate(this.x-width/2,this.y-height/2,this.z-this.z_range/2)
+    rotateX(this.angle)
+    rotateY(this.angle*0.3)
+    stroke(this.stroke_color)
+    texture(this.skin)
+    strokeWeight(this.stroke_weight/(this.size*3))
+    box(volume/this.size)
+    this.angle+=0.01
+  }
+
+  change_skin(skins){
+    let random_number = round(random(skins.length-1))
+    this.skin = skins[random_number]
+  }
+
+
+}
+*/
--- sketch.js
+++ sketch.js
@@ -1,13 +1,114 @@
-function preload() {
-  // put preload code here
+let mySong;
+let duifje;
+let kdb
+let bg
+let hh
+let kdb2
+let kdb3
+let kdb4
+let kdb5
+let kdb6
+let kdb7
+let kdb8
+let lup
+let lup2
+let pidg
+let pidg2
+let pidg3
+let zeb
+let zeb2
+let analyzer;
+let angle=0
+let skins
+let am_skins
+let things=[]
+let teksten=[]
+let n = 15
+let bool = 0
+let movies
+let titles
+let counter=0
+
+function preload(){
+  duifje = loadImage("./assets/duifjewow.jpg")
+  kdb = loadImage("./assets/kerkdakbloem1.2.jpg")
+  mySong = loadSound("./assets/SW_icarus_short.mp3")
+  bg = loadImage("./assets/betongoeie.jpg")
+  hh = loadImage("./assets/hockneyhupla.jpg")
+  kdb2 = loadImage("./assets/kerkdakbloem1.11.jpg")
+  kdb3 = loadImage("./assets/kerkdakbloem1.13.jpg")
+  kdb4 = loadImage("./assets/kerkdakbloem1.16.jpg")
+  kdb5 = loadImage("./assets/kerkdakbloem1.19.jpg")
+  kdb6 = loadImage("./assets/kerkdakbloem1.20.jpg")
+  kdb7 = loadImage("./assets/kerkdakbloem1.26.jpg")
+  kdb8 = loadImage("./assets/kerkdakbloem1.29.jpg")
+  lup = loadImage("./assets/luperd1.6.jpg")
+  lup2 = loadImage("./assets/luperd1.9.jpg")
+  pidg = loadImage("./assets/pidgeon2.7.jpg")
+  pidg2 = loadImage("./assets/pidgeon2.14.jpg")
+  pidg3 = loadImage("./assets/pidgeon2.20.jpg")
+  zeb = loadImage("./assets/zebravent1.6.jpg")
+  zeb2 = loadImage("./assets/zebravent1.11.jpg")
+
+  movies = loadJSON("./assets/movies.json")
 }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
+function setup(){
+
+  createCanvas(windowWidth,windowHeight,WEBGL)
+  analyzer = new p5.Amplitude()
+  analyzer.setInput(mySong)
+  skins = [duifje,kdb,kdb2,kdb3,kdb4,kdb5,kdb6,kdb7,kdb8,lup,lup2,pidg,pidg2,pidg3,zeb,zeb2]
+  am_skins = skins.length
+
+  for(let ii=0;ii<n;ii++){
+    things.push(new Things(skins))
+  }
+
 }
 
-function draw() {
-  // put drawing code here
-  background("red");
+
+
+
+function draw(){
+  background(0)
+  //pointLight(200,0 ,200,200,windowWidth/2,0,0)
+  //pointLight(20,150 ,200,200,0,0,0)
+  var volume = 0
+
+
+
+  mySong.loop()
+  analyzer.toggleNormalize(0)
+  volume = analyzer.getLevel()
+
+  volume = map(volume,0,5,10,80)
+
+
+  for (var jj = 0; jj < n; jj++) {
+    if(volume>72){
+      things[jj].change_skin(skins)
+      counter+=1
+    }
+    if((counter%10) ==1){
+      teksten.push(new Teksten(movies))
+    }
+    for (var kk = 0; kk < teksten.length; kk++) {
+      teksten[kk].display()
+    }
+
+    //things[jj].move()
+    things[jj].display(volume)
+  }
+
+
+  /*rotateX(angle)
+  rotateY(angle*0.3)
+  //fill(frameCount%255,frameCount%255,50)
+  noStroke()
+  texture(skins[1])
+  box(volume)
+
+  angle+=0.01
+*/
 }
